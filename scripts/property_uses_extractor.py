cbf5d21915d5b2e4a69e98ec3b1e3f5df2b8f785
Alessandro Piscopo <ap1a14@soton.ac.uk>
2017-08-31 11:17:20 +0100

    Update property_uses_extractor.py

--- scripts/property_uses_extractor.py
+++ scripts/property_uses_extractor.py
@@ -1,5 +1,3 @@
-#from __future__ import print_function
-#from multiprocessing import Pool
 import os
 import sys
 import re
@@ -30,19 +28,6 @@ def property_names(file_name):
 ###look up properties and write files
 def property_uses(file_name, properties):
     
-    
-    #search for property use
-    #file_name = ('wikidatadumps_1-2013_statement.csv')
-    #with open(file_name, 'rt') as f:
-    #    for line in f:
-    #        for key, value in properties.iteritems():
-    #            property_id = (key+"(?!...)")
-    #            property_file = properties[key]
-    #            if re.search(property_id, line) is not None:
-    #                with open(property_file, 'a') as f:
-    #                    f.write(line)
-    #
-    #
     print file_name
     file_statement = pd.read_csv(file_name, sep = '\t', header = 0,  keep_default_na=False, na_values=[""], error_bad_lines=False)
     
@@ -76,44 +61,19 @@ def file_writer(property_name, file_name):
     
     return foutname
 
-#file_list = []
-#for i in os.listdir(os.getcwd()):
-#    extension = os.path.splitext(i)
-#    if 'csv' in extension[1]:
-#        file_list.append(i)
-#
-#file_property = {}
-#for key, value in file_property.iteritems():
-#    file_property[key] = property_names(key)
-#
-#pool = Pool(processes=4)
-#pool.imap(property_uses, file_property.iteritems())
 
 def main():
     
     file_metadata_1 = sys.argv[1]
-    #file_metadata_2 = sys.argv[2]
-    #file_metadata_3 = sys.argv[3]
-    #file_metadata_4 = sys.argv[4]
-    #
+    
     if not os.path.exists("property_uses"):
         os.mkdir("property_uses")
     
     my_properties_1 = property_names(file_metadata_1)
-    #my_properties_2 = property_names(file_metadata_2)
-    #my_properties_3 = property_names(file_metadata_3)
-    #my_properties_4 = property_names(file_metadata_4)
+    
     
     property_uses(file_metadata_1, my_properties_1)
-    
-    #pool = Pool(4)
-    #
-    ##property_uses #multiprocessing!
-    #pool.apply_async(property_uses, args =(file_metadata_1, my_properties_1))
-    #pool.apply_async(property_uses, args=(file_metadata_2, my_properties_2))
-    #pool.apply_async(property_uses, args=(file_metadata_3, my_properties_3))
-    #pool.apply_async(property_uses, args=(file_metadata_4, my_properties_4))
-
+  
 
 
 if __name__ == "__main__":
